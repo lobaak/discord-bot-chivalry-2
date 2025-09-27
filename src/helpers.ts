13904a391c92ac38bc1bf2367b06549b74c48940
lobaak <lobaak@gmail.com>
2025-09-27 15:16:27 +1000

    Clean-up

---
+++ src/helpers.ts
@@ -0,0 +1,25 @@
+import { REST, Routes } from "discord.js";
+import { ingest } from "./commands";
+
+export async function registerCommands(clientId: string) {
+  const commands = [ingest.command];
+
+  const rest = new REST().setToken(process.env.API_KEY!);
+
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    const data = (await rest.put(
+      Routes.applicationGuildCommands(clientId, process.env.GUILD_ID!),
+      { body: commands }
+    )) as object[];
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,8 @@
-import {
-  Client,
-  Events,
-  GatewayIntentBits,
-  REST,
-  Routes,
-  SlashCommandBuilder,
-} from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 import { ingest } from "./commands";
+import { registerCommands } from "./helpers";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-const GUILD_ID = process.env.GUILD_ID!;
-const TOKEN = process.env.API_KEY!;
-
-const commands = [ingest.command];
-
-async function registerCommands(clientId: string) {
-  const rest = new REST().setToken(TOKEN);
-
-  try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
-
-    const data = (await rest.put(
-      Routes.applicationGuildCommands(clientId, GUILD_ID),
-      { body: commands }
-    )) as object[];
-
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 client.on(Events.InteractionCreate, async (interaction) => {
   ingest.interaction(interaction);
@@ -48,4 +16,4 @@ client.on("ready", async () => {
   await registerCommands(client?.user?.id);
 });
 
-client.login(TOKEN);
+client.login(process.env.API_KEY);
